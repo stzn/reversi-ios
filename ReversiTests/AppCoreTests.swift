e0c59da2d33db9e70e030582ab03bad4445eebeb
stzn <sztk1209@gmail.com>
2020-05-21 08:24:48 +0900

    add test file for preventing build error

---
+++ ReversiTests/AppCoreTests.swift
@@ -0,0 +1,12 @@
+//
+//  AppCoreTests.swift
+//  ReversiTests
+//
+//  Created by Shinzan Takata on 2020/05/21.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+import XCTest
+
+class AppCoreTests: XCTestCase {
+}
